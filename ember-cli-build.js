37db86d1001d6abc13aeebb72415bd3f6698606b
Gregor Woiwode <gw@co-it.eu>
2016-03-15 00:29:37 +0100

    Layouts tweets as cards Adds spartakiade logo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,11 @@ var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
 module.exports = function(defaults) {
   var app = new Angular2App(defaults, {
-    vendorNpmFiles: []
+    vendorNpmFiles: [
+      'bootstrap/dist/css/bootstrap.css',
+      'font-awesome/css/font-awesome.css',
+      'font-awesome/fonts/**'
+    ]
   });
   return app.toTree();
-}
+};
--- src/app/tweets/tweets.service.ts
+++ src/app/tweets/tweets.service.ts
@@ -1,18 +1,14 @@
-import {Injectable}     from 'angular2/core';
-import {Observable}     from 'rxjs/Observable';
+import {Injectable} from 'angular2/core';
+import {Observable} from 'rxjs/Observable';
 import {Http, Headers, RequestOptions} from 'angular2/http';
 import "rxjs/add/operator/map";
 
 @Injectable()
 export class Tweets {
-  private _accessToken: string;
-  private _oAuthUrl = 'https://api.twitter.com/oauth2/token';
-
-  constructor(private _http: Http) {
-  }
+  constructor(private _http: Http) { }
 
   getAll() {
     return this._http.get('http://localhost:3000/api/v1/tweets')
-      .map(res => JSON.parse(res.text()))
+                     .map(res => JSON.parse(res.text()))
   }
 }
