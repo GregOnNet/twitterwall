7dc4e80812bc5f3098c99c37693664728c0ad587
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-03-15 19:24:50 +0100

    Load list of the last *50* Tweets created in the past *48* hours It's not exactly 48 hours but since the day before yesterday.

--- src/app/config.example.ts
+++ src/app/config.example.ts
@@ -2,4 +2,5 @@ export class AppConfig {
   static apiPath = 'http://localhost:3000/api/v1/';
   static search = '#spartakiade';
   static maxAge = 2; //days
+  static maxCount = 50;
 }
--- src/app/tweets/tweets.service.ts
+++ src/app/tweets/tweets.service.ts
@@ -9,8 +9,19 @@ export class Tweets {
   constructor(private _http: Http) { }
 
   getAll() {
-    let params = new URLSearchParams();
-    params.set('q', AppConfig.search);
+    if(AppConfig.maxAge > 0){
+      let d = new Date();
+      d.setTime(d.getTime() - (24*60*60*1000) * AppConfig.maxAge);
+
+      var since = d.getFullYear() + '-' + ('0'+(d.getMonth()+1)).slice(-2) + '-' + d.getDate();
+    }
+
+    var q = AppConfig.search;
+    if(since) q += ' since:' + since;
+
+    var params = new URLSearchParams();
+    params.set('q', encodeURIComponent(q));
+    params.set('count', AppConfig.maxCount.toString());
 
     return this._http.get(AppConfig.apiPath + 'tweets', { search: params })
                      .map(res => res.json());
