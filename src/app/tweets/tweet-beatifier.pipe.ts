5f6ddb94a12a4d8f0aaaa8109be578d9839a3aef
Gregor Woiwode <gw@co-it.eu>
2016-03-15 01:31:47 +0100

    Removes comment

--- src/app/tweets/tweet-beatifier.pipe.ts
+++ src/app/tweets/tweet-beatifier.pipe.ts
@@ -1,13 +1,5 @@
 import {Pipe, PipeTransform} from 'angular2/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 |  exponentialStrength:10}}
- *   formats to: 1024
-*/
+
 @Pipe({name: 'beautifyTweet'})
 export class TweetBeautifier implements PipeTransform {
   transform(value:string) : any {
