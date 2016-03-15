f849e45ce7b2106c9f5d6395357f532be37ce60c
Gregor Woiwode <gw@co-it.eu>
2016-03-15 01:45:21 +0100

    Moves mapping oof tweets to the backend

--- backend/tweets.controller.js
+++ backend/tweets.controller.js
@@ -12,7 +12,31 @@ exports.getAll = function(req, res, next){
   };
 
   request(options, function(err, response, body) {
-    var tweets = JSON.parse(body).statuses;
+    var tweets = JSON.parse(body).statuses.map(tweet);
     res.json(tweets);
   });
+}
+
+function tweet(t) {
+  var has_image = false;
+  var image_url_https;
+
+  if (t.entities !== undefined &&
+      t.entities.media !== undefined &&
+      t.entities.media.length > 0 &&
+      t.entities.media[0].media_url_https !== undefined &&
+      t.entities.media[0].media_url_https.indexOf('.jpg') > 0) {
+    has_image = true;
+    image_url_https = t.entities.media[0].media_url_https;
+  }
+
+  return {
+   creator: t.user.screen_name,
+   text: t.text,
+   created_at: t.created_at,
+   retweet_count: t.retweet_count,
+   favorite_count: t.favorite_count,
+   has_image: has_image,
+   image_url_https: image_url_https
+  };
 }
--- src/app/tweets/tweets.service.ts
+++ src/app/tweets/tweets.service.ts
@@ -9,33 +9,6 @@ export class Tweets {
 
   getAll() {
     return this._http.get('http://localhost:3000/api/v1/tweets')
-                     .map(res => {
-                       console.log(JSON.parse(res.text()));
-                       let tweets = JSON.parse(res.text()).map(tweet => {
-                         let has_image = false;
-                         let image_url_https;
-
-                         if (tweet.entities != undefined &&
-                             tweet.entities.media != undefined &&
-                             tweet.entities.media.length > 0 &&
-                             tweet.entities.media[0].media_url_https != undefined &&
-                             tweet.entities.media[0].media_url_https.indexOf('.jpg') > 0) {
-                           has_image = true;
-                           image_url_https = tweet.entities.media[0].media_url_https;
-                         }
-
-                         return {
-                          creator: tweet.user.screen_name,
-                          text: tweet.text,
-                          created_at: tweet.created_at,
-                          retweet_count: tweet.retweet_count,
-                          favorite_count: tweet.favorite_count,
-                          has_image: has_image,
-                          image_url_https: image_url_https
-                         };
-                       });
-                       console.log(tweets);
-                       return tweets;
-                     })
+                     .map(res => JSON.parse(res.text()));
   }
 }
