c1ca7d78ac81a8e32122c0b389ba6c0f57319998
Gregor Woiwode <gw@co-it.eu>
2016-03-15 01:47:51 +0100

    Documents tweet model used in the app

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ $ npm isntall
 $ node backend/server.js # On 1st console
 $ ng serve               # On 2nd console
 ```
+
+## Models
+
+### Tweet
+
+```json
+"creator": "@user_name"
+"text": "tweet",
+"created_at": "creation_date",
+"retweet_count": 2,
+"favorite_count": 0,
+"has_image": false|true,
+"image_url_https": "https://some-url/image.jpg"
+};
+```
--- backend/tweets.controller.js
+++ backend/tweets.controller.js
@@ -7,7 +7,7 @@ exports.getAll = function(req, res, next){
   var options = {
     url: 'https://api.twitter.com/1.1/search/tweets.json?&result_type=recent&q=%23spartakiade',
     headers: {
-      'Authorization': 'Bearer AAAAAAAAAAAAAAAAAAAAAKyJuAAAAAAAP%2Fl8i%2BIBG4ypJYwbKChIt8fOSOk%3DVuK2E9VgikCCUpmoYV0Zq0ZCE3CeUs3aWryi9TuOnk6Yh9Ykhk'
+      'Authorization': ''
     }
   };
 
